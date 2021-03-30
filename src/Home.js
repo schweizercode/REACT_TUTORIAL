194f57f1d438998016d6337317f2e59a74af61a5
schweizercode <schweizercode@gmail.com>
2021-03-30 15:26:16 +0200

    UseEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
         { title: 'Web dev tool tips ', body: 'lorem ipsum...', author: 'mario', id: 3 },
     ]);
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id != id);
         setBlogs(newBlogs);
@@ -15,13 +17,15 @@ const Home = () => {
 
     useEffect(() => {
         console.log('use effect')
-        console.log(blogs);
-    });
+        console.log(name);
+    }, [name]);
 
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{name}</p>
         </div>
     );
 }
